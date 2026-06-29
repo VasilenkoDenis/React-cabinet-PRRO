aa72e722e10bddea50d8da3c500e67db0546e3e7
VasilenkoDenis <vasilenko397@gmail.com>
2026-06-29 13:55:11 +0300

    Додали провайдер для повідомлень Notistack.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SnackbarProvider } from 'notistack';
 import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
+  <SnackbarProvider>
+
     <App />
+  </SnackbarProvider>
   // </StrictMode>,
 )
