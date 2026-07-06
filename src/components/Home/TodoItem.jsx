1893b15c31a34aa20ceea8280424466808435a85
VasilenkoDenis <vasilenko397@gmail.com>
2026-07-06 14:30:47 +0300

    Підготовили редагування todo-шек.

--- src/components/Home/TodoItem.jsx
+++ src/components/Home/TodoItem.jsx
@@ -4,9 +4,32 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import CheckBox from '@mui/material/Checkbox';
+import TextField from '@mui/material/TextField';
+import { Stack } from '@mui/material';
+import { useState } from 'react';
 
 function TodoItem(props) {
   const todo = props.todo
+ 
+  const [isEdit, setIsEdit] = useState(false);
+  const [title, setTitle] = useState(todo.title ||'');
+  const [description, setDescription] = useState(todo.description || '');
+
+  const toggleIsEdit = () => {
+    setIsEdit(!isEdit);
+    if (isEdit) {
+        props.handleUpdateTodo(todo._id, title, description);
+    }
+  }
+
+  const handleChangeTitle = (event) => {
+    setTitle(event.target.value);
+  }; 
+
+  const handleChangeDescription = (event) => {
+    setDescription(event.target.value);
+  };
+ 
   const handleDeleteTodo = () => {
     props.handleDeleteTodo(todo._id)
   };
@@ -18,16 +41,29 @@ function TodoItem(props) {
   return (
     <Card sx={{ minWidth: 275, backgroundColor:todo.completed ? "lightgreen" : undefined}}>
         <CardContent>
-            <Typography gutterBottom>
-                {todo.title}
-            </Typography>
-            <Typography variant="body2">
-                {todo.description}
-            </Typography>
+            <Stack>
+                {isEdit 
+                    ? <TextField size="small" label="Заголовок завдання" value={title} onChange={handleChangeTitle} />          
+                    :
+                    <Typography gutterBottom>
+                        {todo.title}
+                    </Typography>
+                }
+
+                {isEdit 
+                    ? <TextField size="small" label="Опис завдання" value={description} onChange={handleChangeDescription} />          
+                    :
+                    <Typography variant="body2">
+                        {todo.description}
+                    </Typography>
+                }
+            </Stack>
         </CardContent>
         <CardActions>
             <CheckBox checked={todo.completed} onChange={handleDoneTodo} />
-            <Button size="small">Редагувати</Button>
+            <Button size="small" onClick={toggleIsEdit}>
+                {isEdit ? "Зберегти" : "Редагувати"}
+            </Button>
             <Button size="small" sx={{ backgroundColor: "red", color: "white" }} onClick={handleDeleteTodo}>
                 Видалити
             </Button>
